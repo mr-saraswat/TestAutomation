16a9e9aa9f914f5ef8151c72b652fa14b19724e7
Nitish <nitishsaraswat13101999@gmail.com>
2026-07-10 17:03:40 +0530

    browser property update

--- src/test/java/drivers/driverFactory.java
+++ src/test/java/drivers/driverFactory.java
@@ -1,17 +1,27 @@
 package drivers;
 
+import config.ConfigReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class driverFactory {
     private static WebDriver driver;
+    private static String browser;
     private driverFactory(){
 
     }
     public static WebDriver initDriver()
     {
-        if(driver == null) {
+        browser = ConfigReader.getProperty("browser");
+        if(browser =="chrome" || browser == null);
+        {
             driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        if(browser =="safari");
+        {
+            driver = new SafariDriver();
             driver.manage().window().maximize();
         }
         return  driver;
